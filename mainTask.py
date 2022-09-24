c2c7384132b3f5e4894058cf1247a874a8f11a5e
kulka <kulinska.hanna@gmail.com>
2022-09-24 17:49:53 +0200

    commit w branchu

--- mainTask.py
+++ mainTask.py
@@ -1,17 +1,4 @@
-print("ZADANIE 1.")
+szyfr = (list(map(chr, range(97, 123))))
 
-zakupy= {
-    "piekarnia": ['chleb', 'pączek', 'bułki'],
-    "warzywniak": ['marchew', 'seler', 'rukola']
-}
-for sklep, rzeczy in zakupy.items():    
-    print("Idę do",sklep.capitalize(),", kupuję tu następujące rzeczy: ", [rzecz.capitalize() for rzecz in rzeczy],".")
+print(szyfr[3], szyfr[14],szyfr[1],szyfr[17],szyfr[4],szyfr[6],szyfr[14]," ",szyfr[3],szyfr[13],szyfr[8],szyfr[0])
 
-ilosc = 0
-for sklep in zakupy.items():
-    ilosc+=len(rzeczy)
-
-print("W sumie kupuję ", ilosc, "produktów.")
-#pierwszy commit
-#drugi commit, jakże zaskakujący
-# raz dwa trzy commita robisz ty
