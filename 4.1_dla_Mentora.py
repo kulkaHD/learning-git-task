106e09ffda9a39c8631289564d31f9fa106e3949
kulka <kulinska.hanna@gmail.com>
2022-09-28 23:52:48 +0200

    dokumentacja w definicji

--- 4.1_dla_Mentora.py
+++ 4.1_dla_Mentora.py
@@ -1,4 +1,9 @@
 def czy_palindrom(word):
+    """
+    Returns True/False if an argument is/is not a palindrome.
+    Argument:
+    word (string)
+    """
     for i in range (len(word)):
         if word[i]==word[-(i+1)]:
             return True
