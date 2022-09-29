3d15079e3fe8b697c2b18cfd9d56a12a3325a2e0
kulka <kulinska.hanna@gmail.com>
2022-09-29 10:40:53 +0200

    poprawione zadanie

--- 4.1_dla_Mentora.py
+++ 4.1_dla_Mentora.py
@@ -6,8 +6,9 @@ def czy_palindrom(word):
     """
     for i in range (len(word)):
         if word[i]==word[-(i+1)]:
-            return True
+            x = True  
         else:
-            return False
-
-print(czy_palindrom("potop"))
+            x = False
+            break
+    return x
+print(czy_palindrom("coroc"))
