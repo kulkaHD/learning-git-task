018cf1629d0932005226fa9d515721d6da1cfa27
kulka <kulinska.hanna@gmail.com>
2022-10-03 22:01:23 +0200

    kalkulator.poprawki

--- kalkulator.py
+++ kalkulator.py
@@ -21,116 +21,81 @@ def check_user_input_int(input):
         int(input)
         return True
     except ValueError:
-        return False 
+        return False
 
 from typing import Tuple
-def get_data() -> Tuple[str, float, float]:
+def get_data() -> Tuple[str, list, float]:
     op = input('''Podaj numer operacji:
     1 (dodawanie)
     2 (odejmowanie)
     3 (mnożenie)
     4 (dzielenie) ''')
-    while op != "1" and  op != "2" and  op != "3" and  op != "4":
+    while op not in ("1","2","3","4"):
         print ("Poproszę o liczbę z zakresu 1-4.")
         op = input('''Podaj numer operacji:
     1 (dodawanie)
     2 (odejmowanie)
     3 (mnożenie)
     4 (dzielenie) ''')
-    if op == "1":
-        logging.info("Wybrałeś dodawanie.")
-    elif op == "2":
-        logging.info("Wybrałeś odejmowanie.")
-    elif op == "3":
-        logging.info("Wybrałeś mnożenie.")
-    elif op == "4":
-        logging.info("Wybrałeś dzielenie.")
-    else:
-        pass
 
-    a = input("a = ")
-    while check_user_input_float(a) == False:
-        logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
-        a = input("a = ")
-    a = float(a)
+    op_log_info = {
+        "1": "Wybrałeś dodawanie.",
+        "2": "Wybrałeś odejmowanie.",
+        "3": "Wybrałeś mnożenie.",
+        "4": "Wybrałeś dzielenie."
+        }
+    logging.info(op_log_info[op])
     
-    b = input("b = ")
-    while check_user_input_float(b) == False:
-        logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
-        b = input("b = ")
-    b = float(b)
-  
-    return op, a, b
-
-
-def add(a: float, b: float) -> float:
-    more_numbers = (input("Czy chcesz dodać więcej liczb niż wpisane do tej pory? Wpisz, proszę, tak lub nie: "))
-    while more_numbers.lower() != "tak" and  more_numbers.lower() != "nie":
-        logging.info("Błędna odpowiedź. Poproszę o odpowiedź tak lub nie.")
-        more_numbers = (input("Czy chcesz dodać więcej liczb niż wpisane do tej pory? Wpisz, proszę, tak lub nie: "))
-    if more_numbers.lower() == "tak":
-        how_many_more = (input("Ile argumentów chcesz dodać? "))
-        while check_user_input_int(how_many_more) == False:
+    if op in ("2", "4"):
+        a = input("Wpisz liczbę: ")
+        while check_user_input_float(a) == False:
+            logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
+            a = input("Wpisz liczbę: ")
+        a = float(a)
+    else:
+        a=[]
+        how_many_num = (input("Ile liczb potrzebujesz w swojej operacji? "))
+        while check_user_input_int(how_many_num) == False:
             logging.info("Nie wpisałeś liczby całkowitej. Wpisz liczbę całkowitą.")
-            how_many_more = input("Ile argumentów chcesz dodać? ")
-        how_many_more = int(how_many_more) 
-        all_numbers = [a,b]
-        for i in range (how_many_more):
+            how_many_num = input("Ile liczb potrzebujesz w swojej operacji? ")
+        how_many_num = int(how_many_num) 
+        for i in range (how_many_num-1):
             x = input("Wpisz liczbę: ")
             while check_user_input_float(x) == False:
                 logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
                 x = input("Wpisz liczbę: ")
             x = float(x)
-            all_numbers.append (x)
-        return sum(all_numbers)
-    elif more_numbers.lower() == "nie":
-        return a + b
-    else:
-        pass
-    return a + b
+            a.append (x)
+    
+    b = input("Wpisz liczbę: ")
+    while check_user_input_float(b) == False:
+        logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
+        b = input("Wpisz liczbę: ")
+    b = float(b)
+    while op == "4" and b == 0:
+        logging.warning("Nie można dzielić przez 0. Proszę wybrać inną liczbę.")
+        b = input("Wpisz liczbę: ")
+        while check_user_input_float(b) == False:
+            logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
+            b = input("Wpisz liczbę: ")
+        b = float(b)
+   
+    return op, a, b
+    
+
+def add(a: list, b: float) -> float:
+    return sum(a,b)
 
 def sub(a: float, b: float) -> float:
     return a - b
 
-def mult(a: float, b: float) -> float:
-    more_numbers = (input("Czy chcesz przemnożyć więcej liczb niż wpisałeś do tej pory? Wpisz, proszę, tak lub nie: "))
-    while more_numbers.lower() != "tak" and  more_numbers.lower() != "nie":
-        logging.info("Błędna odpowiedź. Poproszę o odpowiedź tak lub nie.")
-        more_numbers = (input("Czy chcesz dodać więcej liczb niż wpisane do tej pory? Wpisz, proszę, tak lub nie: "))
-    if more_numbers.lower() == "tak":
-        how_many_more = (input("Ile argumentów chcesz dodać? "))
-        while check_user_input_int(how_many_more) == False:
-            logging.info("Nie wpisałeś liczby całkowitej. Wpisz liczbę całkowitą.")
-            how_many_more = input("Ile argumentów chcesz dodać? ")
-        how_many_more = int(how_many_more) 
-        all_numbers = []
-        for i in range (how_many_more):
-            x = input("Wpisz liczbę: ")
-            while check_user_input_float(x) == False:
-                logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
-                x = input("Wpisz liczbę: ")
-            x = float(x)
-            all_numbers.append (x)
-        all_numbers = [3,4]
-        result = a * b
-        for i in range(len(all_numbers)):
-            result *= all_numbers[i]
-        return result
-    elif more_numbers.lower() == "nie":
-        return a * b
-    else:
-        pass      
-    return a * b
+def mult(a: list, b: float) -> float:
+    for i in a:
+        b *=i     
+    return b
 
 
 def div(a: float, b: float) -> float:
-    while b==0:
-        logging.warning("Nie można dzielić przez 0. Proszę wybrać inną liczbę.")
-        b = input("b = ")
-        while check_user_input_float(b) == False:
-            logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
-            b = input("b = ")
-        b = float(b)
     return a / b
 
 operations = {
@@ -138,7 +103,7 @@ operations = {
     "2": sub,
     "3": mult,
     "4": div
-}
+    }
 
 def main() -> float:
     operation, a, b = get_data()
