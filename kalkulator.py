767f5da3e8fb55b288bea854426282b0eed1b096
kulka <kulinska.hanna@gmail.com>
2022-10-05 20:34:33 +0200

    kalkulator.poprawki cd

--- kalkulator.py
+++ kalkulator.py
@@ -9,6 +9,21 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+def add(a, b, *args):
+    return a + b + sum(args)
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b, *args):
+    result = a * b
+    for i in args:
+        result *= i
+    return result
+
+def div(a, b):
+    return a / b
+
 def check_user_input_float(input):
     try:
         float(input)
@@ -23,50 +38,53 @@ def check_user_input_int(input):
     except ValueError:
         return False
 
-from typing import Tuple
-def get_data() -> Tuple[str, list, float]:
-    op = input('''Podaj numer operacji:
-    1 (dodawanie)
-    2 (odejmowanie)
-    3 (mnożenie)
-    4 (dzielenie) ''')
-    while op not in ("1","2","3","4"):
-        print ("Poproszę o liczbę z zakresu 1-4.")
-        op = input('''Podaj numer operacji:
-    1 (dodawanie)
-    2 (odejmowanie)
-    3 (mnożenie)
-    4 (dzielenie) ''')
+operations = {
+    "1": add,
+    "2": sub,
+    "3": mult,
+    "4": div
+    }
 
-    op_log_info = {
+operations_instruction = '''Podaj numer operacji:
+    1 (dodawanie)
+    2 (odejmowanie)
+    3 (mnożenie)
+    4 (dzielenie) '''
+
+op_log_info = {
         "1": "Wybrałeś dodawanie.",
         "2": "Wybrałeś odejmowanie.",
         "3": "Wybrałeś mnożenie.",
         "4": "Wybrałeś dzielenie."
         }
+
+def get_data():
+    op = input(operations_instruction)
+    while op not in operations:
+        print ("Poproszę o liczbę z zakresu 1-4.")
+        op = input(operations_instruction)  
     logging.info(op_log_info[op])
-    
-    if op in ("2", "4"):
-        a = input("Wpisz liczbę: ")
-        while check_user_input_float(a) == False:
-            logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
-            a = input("Wpisz liczbę: ")
-        a = float(a)
-    else:
-        a=[]
+    args=[]
+    if op in ("1", "3"):
         how_many_num = (input("Ile liczb potrzebujesz w swojej operacji? "))
         while check_user_input_int(how_many_num) == False:
             logging.info("Nie wpisałeś liczby całkowitej. Wpisz liczbę całkowitą.")
             how_many_num = input("Ile liczb potrzebujesz w swojej operacji? ")
         how_many_num = int(how_many_num) 
-        for i in range (how_many_num-1):
+        for i in range (how_many_num-2):
             x = input("Wpisz liczbę: ")
             while check_user_input_float(x) == False:
                 logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
                 x = input("Wpisz liczbę: ")
             x = float(x)
-            a.append (x)
-    
+            args.append(x)
+      
+    a = input("Wpisz liczbę: ")
+    while check_user_input_float(a) == False:
+        logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
+        a = input("Wpisz liczbę: ")
+    a = float(a)
+       
     b = input("Wpisz liczbę: ")
     while check_user_input_float(b) == False:
         logging.info("Nie wpisałeś liczby. Wpisz liczbę.")
@@ -80,36 +98,12 @@ def get_data() -> Tuple[str, list, float]:
             b = input("Wpisz liczbę: ")
         b = float(b)
    
-    return op, a, b
+    return op, a, b, args
     
-
-def add(a: list, b: float) -> float:
-    return sum(a,b)
-
-def sub(a: float, b: float) -> float:
-    return a - b
-
-def mult(a: list, b: float) -> float:
-    for i in a:
-        b *=i     
-    return b
-
-def div(a: float, b: float) -> float:
-    return a / b
-
-operations = {
-    "1": add,
-    "2": sub,
-    "3": mult,
-    "4": div
-    }
-
-def main() -> float:
-    operation, a, b = get_data()
-    result = operations[operation](a, b)
+def main():
+    operation, a, b, args = get_data()
+    result = operations[operation](a, b, *args)
     print ("wynik to: ", "%.2f" % result)
-    if __name__ == "__main__":
-        print ("Jaki fajny kalkulator,ciekawe,czy ktos w innym programie go będzie uzywał?")
     return result
 
 main()
